6ee2726ed47da0a79b640c6d04347c1bc053438e
gangadhar kadam <gangadhar.k@gmail.com>
2014-12-17 14:13:33 +0530

    for push upstream

--- celio/celio/page/support_dashboard/support_dashboard.js
+++ celio/celio/page/support_dashboard/support_dashboard.js
@@ -1,7 +1,116 @@
-frappe.pages['support-dashboard'].onload = function(wrapper) {
+frappe.require('assets/js/chart.js');
+frappe.pages['support-dashboard'].onload = function(wrapper) { 
 	frappe.ui.make_app_page({
 		parent: wrapper,
-		title: 'Support Dashboard',
+		title: 'Dashbaord',
 		single_column: true
 	});
-}
+$(' <div id="salesmain" style="height:10%; width:100%;" >\
+	<table class="table table-bordered" style="height:20px; width:100%;border-radius:10px;background-color: #f9f9f9;">\
+	<tr width="100%">\
+	<td width="50%"><div class="browcl" id ="brow"  style="min-height: 50px;"><b></div></td>\
+	<td width="50%"><div class="crowcl" id ="crow"  style="min-height: 50px;"><b></div></td>\
+	</tr>\
+	</table>\
+	<div id="head" style="height:100%; width:100%;"><b>Branch-wise Support Tickets</tr></table></div><div id="salesmain" style="height:100%; width:100%;" >\
+	<table class="table table-bordered" style="height:200px; width:100%;border-radius:10px;background-color: #f9f9f9;">\
+	<tr width="100%">\
+	<td width="100%"><div class="grossrowcl" id ="grossrow"  style="min-height: 50px;"></div></td>\
+	</tr>\
+	</table>\
+	<div id="head" style="height:100%; width:100%;"><b>Pending Support Tickets Summary</tr></table></div><div id="salesmain" style="height:100%; width:100%;" >\
+	<table class="table table-bordered" style="height:200px; width:100%;border-radius:10px;background-color: #f9f9f9;">\
+	<tr width="100%">\
+	<td width="100%"><div class="arowcl" id ="arow"  style="min-height: 50px;"></div></td>\
+	</tr>\
+	</div>\
+	</table>').appendTo($(wrapper).find('.layout-main'));
+
+	new frappe.Dashboard(wrapper);
+
+}	
+
+frappe.Dashboard = Class.extend({
+	init: function(wrapper) {
+		this.wrapper = wrapper;
+		this.body = $(this.wrapper).find(".layout-main");
+		this.open_closed_sp();
+		this.pending_sp();
+		this.support_list();
+		this.support_report();
+		},
+
+	open_closed_sp:function(){
+			// console.log(user);
+			var me = this;			
+		    frappe.call({
+			method:"celio.celio.page.support_dashboard.support_dashboard.get_supp_tkt",
+			args:{'user':user},
+			callback: function(r) {
+			console.log(r.message);
+			var options = {packages: ['corechart'], callback : drawChart};
+		    google.load('visualization', '1', options);
+		    function drawChart() {
+		  	mydata=[['Channel', 'Open Tickets', 'Closed Tickets']];
+		  	 for(var x in r.message.supp_tckt){
+  				mydata.push(r.message.supp_tckt[x]);
+               }
+            var data = google.visualization.arrayToDataTable(mydata);
+		    var options = {
+		      hAxis: {title: 'No of Tickets',titleTextStyle: {color: '#009933'}},
+		      vAxis: {title: 'Department',minValue:0,titleTextStyle: {color: '#009933'}},
+		      width: 920,
+        	  height:300,
+        	  legend: { position: 'top', maxLines: 3 },
+        	  isStacked: true        	 
+		    };
+		    $(me.wrapper).find('.grossrowcl').empty();
+		    var chart = new google.visualization.BarChart(document.getElementById("grossrow"));
+		    chart.draw(data, options);
+		    }
+		    }
+	    });
+	},
+
+	pending_sp:function(){
+			console.log("in make grosss");
+			var me = this;			
+		    frappe.call({
+			method:"celio.celio.page.support_dashboard.support_dashboard.get_pending_sp",
+			args:{'user':user},
+			callback: function(r) {
+			console.log(r.message);
+			var options = {packages: ['corechart'], callback : drawChart};
+		    google.load('visualization', '1', options);
+		    function drawChart() {
+		  	mydata=[['Tickets','Pending Tickets(In days)']];
+		  	 for(var x in r.message.pending_sp){
+  				mydata.push(r.message.pending_sp[x]);
+               }
+            var data = google.visualization.arrayToDataTable(mydata);
+		    var options = {
+		      hAxis: {title: 'No.of pending tickets',titleTextStyle: {color: '#009933'}},
+		      vAxis: {title: 'User',minValue:0,titleTextStyle: {color: '#009933'}},
+		      width: 920,
+        	  height:350,
+        	  legend: { position: 'top', maxLines: 3 }
+		    };
+		    $(me.wrapper).find('.arowcl').empty();
+		    var chart = new google.visualization.BarChart(document.getElementById("arow"));
+		    chart.draw(data, options);
+		    }
+		    }
+	    });
+	},
+
+	support_list:function(){
+			var me = this;
+			$("#brow").html('<a href="../desk#List/Support Ticket">Support Ticket List</a>')
+	},
+
+	support_report:function(){
+			var me = this;
+			$("#crow").html('<a href="../desk#Report/Support Ticket">Support Ticket Report</a>')
+	},
+
+	});
--- celio/celio/page/support_dashboard/support_dashboard.py
+++ celio/celio/page/support_dashboard/support_dashboard.py
@@ -4,52 +4,127 @@ import frappe
 def get_escalation_for_supportticket():
   from frappe.utils import cstr
   from frappe.utils.email_lib import sendmail
-  frappe.errprint("Hii")
-  # print "hiii"
-   #      aa="select distinct(subdate(CURDATE(), 1)) from `tabHoliday` where subdate(CURDATE(), 1) not in (select holiday_date from `tabHoliday` where parent='2014-2015/Maharashtra/001')"
-   #      res=frappe.db.sql(aa)
-   #      j=0
-   #      if res:
-   #             for i in range (2,15):
-			# bb="select distinct(subdate(CURDATE(), "+cstr(i)+")) from `tabHoliday`"
-   #                      #print bb
-			# res1=frappe.db.sql(bb)
-			# if res1:
-			#   cc="select distinct(subdate(CURDATE(), 1)) from `tabHoliday` where '"+cstr(res1[0][0])+"' in (select holiday_date from `tabHoliday` where parent='2014-2015/Maharashtra/001')"
-			#   #print cc
-			#   res2=frappe.db.sql(cc)
-			#   if res2:
-   #    			      j=j+24
-			#   else:
-   #                           print "breaning"
-			#      break
+  # frappe.errprint("Hii")
+  # print "Hello"
+  # msg="support ticket creation"
+  # sendmail('priya.s@indictranstech.com', subject="Create Supp.Ticket", msg=msg)
+  
   j = 0       
   from frappe.utils import get_first_day, get_last_day, add_to_date, nowdate, getdate
   #qry1="select name from `tabSupport Ticket` t where t.status='Open' and t.creation < DATE_SUB(NOW(), INTERVAL 24+"+cstr(j)+" HOUR) AND  t.creation > DATE_SUB(NOW(), INTERVAL 48+"+cstr(j)+" HOUR)"
   qry=frappe.db.sql("select name from `tabSupport Ticket` t where t.status='Open' and t.creation < DATE_SUB(NOW(), INTERVAL 24+"+cstr(j)+" HOUR) AND  t.creation > DATE_SUB(NOW(), INTERVAL 48+ '%s' HOUR)",(j),as_list=1)
   frappe.errprint("in 24 "+cstr(qry))
-  msg="Hello, Support ticket '"+cstr(qry)+"' is assigned to you.."
-  sendmail('priya.s@indictranstech.com', subject="Support Ticket Escalation", msg = msg)
+  
   if qry:
-    for [k] in qry:
-      frappe.errprint(k)
-      p=frappe.db.sql("select territory from `tabSupport Ticket` where name=%s",(k),as_list=1)
-      frappe.errprint(p)
-      w=frappe.db.sql("select parent from `tabDefaultValue` where  defkey = '%s' and defvalue = '%s'"%('territory',p[0][0]))
-      frappe.errprint(w[0][0])
-      ee="update `tabSupport Ticket` set assigned_to='"+cstr(w[0][0])+"' where name='"+cstr(k)+"'"
-      frappe.db.sql(ee)
+    for [st] in qry:
+      region=frappe.db.sql("select territory from `tabSupport Ticket` where name=%s",(st),as_list=1)
+      regional_head=frappe.db.sql("select parent from `tabDefaultValue` where  defkey = '%s' and defvalue = '%s'"%('territory',region[0][0]))
+      frappe.errprint(region[0][0])
+      sup_tkt=frappe.db.sql("update `tabSupport Ticket` set assigned_to='%s' where name='%s'"%(regional_head[0][0],st),as_list=1)
       frappe.db.commit()
-      frappe.errprint("Updated")
+      flg=frappe.db.sql("select flag from `tabSupport Ticket` where name=%s",(st),as_list=1)
+      # frappe.errprint(flg[0][0])
+      if flg[0][0]=='not' and len(regional_head[0]) > 0:
+        msg=frappe.db.sql("select template from `tabEmail Template` where template_type='Escalation'",as_list=1)
+        sendmail(regional_head[0][0], subject="Support Ticket Escalation", msg = msg[0][0])
+        flg1=frappe.db.sql("update `tabSupport Ticket` set flag='fst' where name=%s",(st),as_list=1)
+        frappe.db.commit()
 
-  qr=frappe.db.sql("select name from `tabSupport Ticket` t where t.status='Open' and  t.creation < DATE_SUB(NOW(), INTERVAL 48+"+cstr(j)+" HOUR) AND t.creation > DATE_SUB(NOW(), INTERVAL 72+ '%s' HOUR)",(j),as_list=1)
+  qr=frappe.db.sql("select name from `tabSupport Ticket` t where t.status='Open' and  t.creation < DATE_SUB(NOW(), INTERVAL 48+"+cstr(j)+" HOUR) AND t.creation > DATE_SUB(NOW(), INTERVAL 100+ '%s' HOUR)",(j),as_list=1)
   frappe.errprint("in 48 "+cstr(qr))
   if qr:
-    for [l] in qr:
-      frappe.errprint(l)
-      q=frappe.db.sql("Select p.name from `tabProfile` p, `tabUserRole` r where r.role='Support Manager' and r.parent=p.name")
-      ff="update `tabSupport Ticket` set assigned_to_higher_level='"+cstr(q[0][0])+"' where name='"+cstr(l)+"'"
-      frappe.db.sql(ff)
+    for [st1] in qr:
+      frappe.errprint(st1)
+      # supp_manager=frappe.db.sql("select parent from `tabDefaultValue` where  defkey = '%s' and defvalue = 'Support Manager'"%('role'))
+      supp_manager=frappe.db.sql("select parent from `tabUserRole` where role='Support Manager' and parent!='Administrator'")
+      frappe.errprint(supp_manager[0][0])
+      sup_tkt1=frappe.db.sql("update `tabSupport Ticket` set assigned_to='%s' where name='%s'"%(supp_manager[0][0],st1),as_list=1)
       frappe.db.commit()
-      sendmail('priya.s@indictranstech.com', subject="Support Ticket Escalation", msg = msg)
-      frappe.errprint("Updated")
+      flg1=frappe.db.sql("select flag from `tabSupport Ticket` where name=%s",(st1),as_list=1)
+      # frappe.errprint(flg1[0][0])
+      if flg1[0][0]=='fst':
+        msg=frappe.db.sql("select template from `tabEmail Template` where template_type='Escalation'",as_list=1)
+        sendmail(supp_manager[0][0], subject="Support Ticket Escalation", msg = msg[0][0])
+        f=frappe.db.sql("update `tabSupport Ticket` set flag='snd' where name=%s",(st1),as_list=1)
+        frappe.db.commit()
+
+
+@frappe.whitelist()
+def get_supp_tkt(user):
+  frappe.errprint("on dashboard")
+  role=frappe.db.sql("select name from `tabRole`", as_list=1)
+  for [r] in role:
+    rl=frappe.db.sql("select role from `tabUserRole` where parent=%s",(user),as_list=1)
+    # frappe.errprint(rl[0][0])
+    if rl[0][0]=='Helpdesk User':
+      supp_tckt=frappe.db.sql("""select priority,sum(case when status='Open' then _count else 0 end) 
+        as open_count,sum(case when status='Closed' then _count else 0 end) as close_count 
+        from (select priority,status,count(*) as _count from `tabSupport Ticket` 
+        where creation > DATE_SUB(CURDATE(), INTERVAL 1 DAY) group by  priority,status)foo 
+        group by priority""",as_list=1)
+      ret={'supp_tckt' : supp_tckt}
+      return ret
+
+    elif rl[0][0]=='Support User':
+      supp_tckt=frappe.db.sql("""select priority,sum(case when status='Open' then _count else 0 end) 
+        as open_count,sum(case when status='Closed' then _count else 0 end) as close_count 
+        from (select priority,status,count(*) as _count from `tabSupport Ticket` 
+        where creation > DATE_SUB(CURDATE(), INTERVAL 1 DAY) group by  priority,status)foo 
+        group by priority""",as_list=1)
+      ret={'supp_tckt' : supp_tckt}
+      return ret
+
+    elif rl[0][0]=='Support Regional Head':
+      supp_tckt=frappe.db.sql("""select name,(select count(*) from `tabSupport Ticket` where 
+          status='Open' and creation > DATE_SUB(CURDATE(), INTERVAL 1 DAY) and branch_id=b.name) 
+          as Open_Count, (select count(*) from `tabSupport Ticket` where status='Closed' and 
+          creation > DATE_SUB(CURDATE(), INTERVAL 1 DAY) and branch_id=b.name) as Close_Count
+          from `tabCelio Department` b""",as_list=1)
+      ret={'supp_tckt' : supp_tckt}
+      return ret
+
+    elif rl[0][0]=='Support Manager':
+      # frappe.errprint("rl")
+      supp_tckt=frappe.db.sql("""select territory,(select count(*) from `tabSupport Ticket` where 
+        status='Open' and creation > DATE_SUB(CURDATE(), INTERVAL 1 DAY) and territory=b.territory) 
+        as Open_Count, (select count(*) from `tabSupport Ticket` where status='Closed' and 
+        creation > DATE_SUB(CURDATE(), INTERVAL 1 DAY) and territory=b.territory) as Close_Count
+        from `tabCelio Department` b""",as_list=1)
+      ret={'supp_tckt' : supp_tckt}
+      return ret
+
+
+@frappe.whitelist()
+def get_pending_sp(user):
+  role=frappe.db.sql("select name from `tabRole`", as_list=1)
+  for [r] in role:
+    rl=frappe.db.sql("select role from `tabUserRole` where parent=%s",(user),as_list=1)
+    branch=frappe.db.sql("select distinct defvalue from `tabDefaultValue` where defkey='celio department' and parent=%s",(user),as_list=1)
+    territory=frappe.db.sql("select distinct defvalue from `tabDefaultValue` where defkey='territory' and parent=%s",(user),as_list=1)
+    frappe.errprint(branch)
+    if rl[0][0]=='Helpdesk User':
+      pending_sp=frappe.db.sql("""select assigned_to,count(name) from `tabSupport Ticket` where 
+         creation < DATE_SUB(CURDATE(), INTERVAL 1 DAY) and status <>'Closed' and branch_id=%s group by assigned_to""",(branch[0][0]),as_list=1)
+      ret={'pending_sp' : pending_sp}
+      return ret
+
+    elif rl[0][0]=='Support User':
+      pending_sp=frappe.db.sql("""select assigned_to,count(name) from `tabSupport Ticket` where 
+         creation < DATE_SUB(CURDATE(), INTERVAL 1 DAY) and status <>'Closed' and branch_id=%s group by assigned_to""",(branch[0][0]),as_list=1)
+      ret={'pending_sp' : pending_sp}
+      return ret
+
+    elif rl[0][0]=='Support Regional Head':
+      pending_sp=frappe.db.sql("""select assigned_to,count(name) from `tabSupport Ticket` where 
+         creation < DATE_SUB(CURDATE(), INTERVAL 1 DAY) and status <>'Closed' and territory=%s group by assigned_to""",(territory[0][0]),as_list=1)
+      ret={'pending_sp' : pending_sp}
+      return ret
+
+    elif rl[0][0]=='Support Manager':
+      pending_sp=frappe.db.sql("""select territory,count(name) from `tabSupport Ticket` where 
+         creation < DATE_SUB(CURDATE(), INTERVAL 1 DAY) and status <>'Closed' group by territory""",as_list=1)
+      ret={'pending_sp' : pending_sp}
+      return ret
+
+
+
